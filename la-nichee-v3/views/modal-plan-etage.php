fe7f4598de6810136e47d75bd2124da1a37c2eb3
xdNoliv <oliviermairet@gmail.com>
2016-01-23 16:04:21 +0100

    delete la-nichee-v3 folder, no longer usefull

--- la-nichee-v3/views/modal-plan-etage.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-	<div class="col-md-12 modal-img">
-		<img src="img/etage.jpg"/>
-	</div>
-</div>
--- la-nichee-v3/views/modal-plan-rdc-02.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-	<div class="col-md-12 modal-img">
-		<img src="img/rdc-02.jpg"/>
-	</div>
-</div>
